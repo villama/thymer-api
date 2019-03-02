8ee483479d766b6ccd3e17202c2c98d18ad1d07f
Julius Villamayor <juliusvillamayor@gmail.com>
2019-03-02 02:15:10 -0800

    Bug fix 4real

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require("express")
 const app = express()
-const port = 3000
 
 app.get("/", (req, res) => {
   return res.send("Hello Cara!")
 })
 
-app.listen(port, () =>
-  console.log(`thymer app listening on port ${process.env.PORT || port}`)
+app.listen(process.env.PORT || 5000, () =>
+  console.log(`thymer app listening on port ${process.env.PORT || 5000}`)
 )
