8a71a910dda7c4c14910489f75c9aed0c460c425
Julius Villamayor <juliusvillamayor@gmail.com>
2019-03-27 11:44:54 -0700

    joi regex

--- index.js
+++ index.js
@@ -18,28 +18,25 @@ app.post("/register", (req, res) => {
     console.log(joi.error.message)
     return res.status(422).send(joi.error.message)
   }
-  // Validate username
-  if (!req.body.username.match("^[a-zA-Z0-9]+(?:[_-]?[a-zA-Z0-9])*$")) {
-    console.log("Illegal username format")
-    return res.status(422).send("Illegal username format")
-  }
 
   const { username, email, password, first_name, last_name } = req.body
 
   // Hash password
   const hash = bcrypt.hashSync(password, bcrypt.genSaltSync(13))
+
   // Insert user
   query(
     "insert into accounts (username, email, password, first_name, last_name, created_on) values ($1, $2, $3, $4, $5, current_timestamp)",
     [username, email, hash, first_name.trim(), last_name.trim()],
     (err, result) => {
       if (err) return res.status(500).send(err.detail ? err.detail : err)
+
       // Return user
       query(
         "select * from accounts where lower(username) = lower($1)",
         [username],
         (err, result) => {
-          if (err) return res.status(500)
+          if (err) return res.status(500).send(err.detail ? err.detail : err)
           var user = result.rows[0]
           delete user["password"]
           console.log("User " + user.username + " created")
--- validate.js
+++ validate.js
@@ -2,7 +2,7 @@ const joi = require("joi")
 
 function register(input) {
   return joi.validate(input, {
-    username: joi.string().min(3).max(50).required(),
+    username: joi.string().min(3).max(50).regex(/^[a-zA-Z0-9]+(?:[_-]?[a-zA-Z0-9])*$/).required(),
     email: joi.string().email({ minDomainAtoms: 2 }).required(),
     password: joi.string().min(6).max(200).strip().required(),
     first_name: joi.string().min(1).max(100).required(),
